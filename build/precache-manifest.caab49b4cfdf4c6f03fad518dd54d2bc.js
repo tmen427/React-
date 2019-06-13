ab221bc1e8405c7acfdc195f8c573f028072eeed
tony <tmonnie@stp-er-10-129-54-7.wireless.umn.edu>
2019-06-13 18:12:11 -0500

    update1

---
+++ build/precache-manifest.caab49b4cfdf4c6f03fad518dd54d2bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2840ca780ba64086cebea3c7bb7a5b6",
+    "url": "/React-/index.html"
+  },
+  {
+    "revision": "f9b08d2d7d8847b1b352",
+    "url": "/React-/static/css/main.7e2f8446.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/React-/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "f9b08d2d7d8847b1b352",
+    "url": "/React-/static/js/main.cd7b7926.chunk.js"
+  },
+  {
+    "revision": "ed8797ec564c78280e29",
+    "url": "/React-/static/js/runtime~main.fd6a6a40.js"
+  }
+]);
